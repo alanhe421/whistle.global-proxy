664c9eff42ce9be0952c93e3c8a8cdc393ce658d
Alan He <qianghe421@gmail.com>
2022-10-04 15:01:03 +0800

    fixed #1

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const program = require('commander');
 const pjson = require('../package.json');
-const {enableProxy, disableProxy} = require('set-global-proxy');
-
+const {enableProxy, disableProxy, getCurProxy} = require('@stacker/set-global-proxy');
+const readline = require('readline');
+const chalk = require('chalk');
 program
   .command(`set`)
   .option('--host <string>', 'Whistle地址', '127.0.0.1')
@@ -22,13 +23,34 @@ program.version(pjson.version);
 program.parse(process.argv);
 
 function set(options) {
-  if (enableProxy({
-    host: options.host, port: +options.port, // bypass?: ;
-    sudo: true
-  })) {
-    console.log('Whistle作为系统代理，设置成功🎉');
+  const currentProxyConfig = getCurProxy();
+  const doSetting = () => {
+    if (enableProxy({
+      host: options.host, port: +options.port, // bypass?: ;
+      sudo: true
+    })) {
+      console.log(chalk.blue('Whistle作为系统代理，设置成功，🎉，配置如下'));
+      console.log(chalk.blue(JSON.stringify(getCurProxy())));
+    } else {
+      console.log(chalk.red('Whistle作为系统代理，设置失败，❌'));
+    }
+  }
+
+  if (currentProxyConfig.http.host.trim()
+    || currentProxyConfig.https.host.trim()) {
+    console.log(chalk.blue(`系统代理已存在，配置如下`));
+    console.log(chalk.blue(`${JSON.stringify(currentProxyConfig)}\n`));
+    const rl = readline.createInterface({
+      input: process.stdin, output: process.stdout
+    });
+    rl.question('回车即覆盖该配置[enter or no]', function (name) {
+      if (name !== 'no') {
+        doSetting();
+      }
+      rl.close();
+    });
   } else {
-    console.log('Whistle作为系统代理，设置失败,❌');
+    doSetting();
   }
 }
 
